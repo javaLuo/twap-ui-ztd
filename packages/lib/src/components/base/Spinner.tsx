f8520d341f512068c22de404e723914e72b6cb5d
javaLuo <376693576@qq.com>
2025-02-26 23:19:32 +0800

    up

--- packages/lib/src/components/base/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-import { CircularProgress } from "@mui/material";
-import React from "react";
-
-function Spinner({ className }: { className?: string }) {
-  return <CircularProgress className={`twap-spinner ${className}`} />;
-}
-
-export default Spinner;
--- packages/lib/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * as Base from "./base";
-export * as Labels from "./Labels";
-export * from "./Components";
-export * from "./Wizard";
-export * as Orders from "./OrdersComponents";
